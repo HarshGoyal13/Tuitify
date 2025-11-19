16272e34865f9213a1b9de3909899376802c47ae
Harsh Goyal <harshgoyal0219@gmail.com>
2025-11-19 15:48:51 +0530

    fix issue

--- Backend/Config/SendMail.js
+++ Backend/Config/SendMail.js
@@ -1,11 +1,19 @@
 const { Resend } = require("resend");
-const dotenv = require('dotenv')
-dotenv.config()
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-await resend.emails.send({
-  from: "no-reply@yourdomain.com",
-  to: email,
-  subject: subject,
-  html: html,
-});
+const SendMail = async ({ email, subject, html }) => {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",  // or verified domain
+      to: email,
+      subject,
+      html,
+    });
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Email failed:", error);
+  }
+};
+
+module.exports = SendMail;
