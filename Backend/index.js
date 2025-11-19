7a291d807bfbcf2efb870200c05c15363b147224
Harsh Goyal <harshgoyal0219@gmail.com>
2025-11-19 15:19:19 +0530

    fix issue

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-const contactRoute = require("./Routes/Contact");
+const contactRoute = require("./routes/Contact");
 
 const cors = require('cors');
 
